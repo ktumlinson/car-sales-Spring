bc60bfae9f1f58de47ff0722f83aa89c644ce8b6
Kyle Tumlinson <tumlinsonkyle@gmail.com>
2022-09-12 08:46:47 -0500

    Initial creation

---
+++ car-sales/src/main/java/com/revature/CarSalesApplication.java
@@ -0,0 +1,13 @@
+package com.revature;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarSalesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarSalesApplication.class, args);
+	}
+
+}
